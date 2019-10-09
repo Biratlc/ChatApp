b9674393b50b39ecd1634dee0351a3f5b04f4af7
biratlamichhane <lcbirat@gmail.com>
2019-10-09 18:43:15 -0500

    Done with one group chat room.

--- selu-chatbox/src/App.js
+++ selu-chatbox/src/App.js
@@ -1,38 +1,135 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import useLocalStorage from 'react-use-localstorage';
+import useSocket from 'use-socket.io-client';
+import { useImmer } from 'use-immer';
+import { useOnlineStatus, useWindowSize } from '@withvoid/melting-pot';
+import useClippy from 'use-clippy';
+
 import './index.css';
 
+const Messages = props => {
+    const [ clipboard, setClipboard ] = useClippy();
+
+    return props.data.map(m => m[0] !== '' ?
+        (<li><strong>{m[0]}</strong> :<a onClick={()=>{setClipboard(`${m[1]}`)}} href="#"><i style={{float:'right',color:'black'}} class=" material-icons">content_copy</i></a> <div className="innermsg">{m[1]}</div></li>)
+        : (<li className="update">{m[1]}</li>) );
+}
+
+const Online = props => props.data.map(m => <li id={m[0]}>{m[1]}</li>)
+
 export default () => {
-    const [id, setId] = useState("");
-    const [nameInput, setNameInput] = useState("");
-    const [room, setRoom] = useState("");
+    const [room, setRoom] = useLocalStorage('room','');
+    const [id, setId] = useLocalStorage('id', '');
+
+    const [socket] = useSocket('https://open-chat-naostsaecf.now.sh');
+
+    const [messages, setMessages] = useImmer([]);
+
+    const [onlineList, setOnline] = useImmer([]);
+
+    const { online } = useOnlineStatus();
+    const { width } = useWindowSize();
+
+    useEffect(()=>{
+        socket.connect();
+
+        if(id){
+            socket.emit('join',id,room);
+        }
+
+        socket.on('message que',(nick,message) => {
+            setMessages(draft => {
+                draft.push([nick,message])
+            })
+        });
+
+        socket.on('update',message => setMessages(draft => {
+            draft.push(['',message]);
+        }))
+
+        socket.on('people-list',people => {
+            let newState = [];
+            for(let person in people){
+                newState.push([people[person].id,people[person].nick]);
+            }
+            setOnline(draft=>{draft.push(...newState)});
+        });
+
+        socket.on('add-person',(nick,id)=>{
+            setOnline(draft => {
+                draft.push([id,nick])
+            })
+        })
+
+        socket.on('remove-person',id=>{
+            setOnline(draft => draft.filter(m => m[0] !== id))
+        })
+
+        socket.on('chat message',(nick,message)=>{
+            setMessages(draft => {draft.push([nick,message])})
+        })
+    },0);
 
     const handleSubmit = e => {
         e.preventDefault();
-        if (!nameInput) {
+        const name = document.querySelector('#name').value.trim();
+        const room_value = document.querySelector('#room').value.trim();
+        console.log(name);
+        if (!name) {
             return alert("Name can't be empty");
         }
         setId(name);
-        socket.emit("join", name, room);
+        setRoom(room_value);
+        socket.emit("join", name,room_value);
     };
 
+    const handleSend = e => {
+        e.preventDefault();
+        const input = document.querySelector('#m');
+        if(input.value.trim() !== ''){
+            socket.emit('chat message',input.value,room);
+            input.value = '';
+        }
+    }
+
+    const logOut = () => {
+        socket.disconnect();
+        setOnline(draft=>[]);
+        setMessages(draft=>[]);
+        setId('');
+        socket.connect();
+    }
+
     return id !== '' ? (
-        <div>Hello</div>
+        <section style={{display:'flex',flexDirection:'row'}} >
+            <ul id="messages"><Messages data={messages} /></ul>
+            <ul id="online"> <a onClick={()=>logOut()} href='#'><div style={{float:'right'}}></div></a> {online ? '️ You are Online' : 'You are Offline'} <hr/><Online data={onlineList} /> </ul>
+            <div id="sendform">
+                <form onSubmit={e => handleSend(e)} style={{display: 'flex'}}>
+                    <input id="m" />
+                    {width > 1000 ? <button style={{width:'100px'}} type="submit">Send Message</button> :
+                        <button style={{width:'50px'}}><i style={{fontSize:'15px'}} class="material-icons">send</i></button>}
+                </form>
+            </div>
+        </section>
     ) : (
-        <div style={{ textAlign: "center", margin: "30vh auto", width: "70%" }}>
+        <div style={{ textAlign: 'center', margin: '30vh auto', width: '70%' }}>
             <form onSubmit={event => handleSubmit(event)}>
-                <input
-                    id="name"
-                    onChange={e => setNameInput(e.target.value.trim())}
-                    required
-                    placeholder="What is your name .."
-                />
-                <br />
-                <input
-                    id="room"
-                    onChange={e => setRoom(e.target.value.trim())}
-                    placeholder="What is your room .."
-                />
-                <br />
+                <input id="name" required placeholder="What's your name ?" /><br />
+
+                <input id="room" placeholder="What's your Major ?" /><br />
+
+
+      {/*<label for=" sel-options">What's your Major ?"</label>*/}
+      {/*<select id=" sel-options" class=" form-control">*/}
+         {/*<option value="">Select Class</option>*/}
+         {/*<option id="room" value="CMPS 411">CMPS 411</option>*/}
+         {/*<option id="room" value="Mathematics">Mathematics</option>*/}
+
+      {/*</select>*/}
+
+
+
                 <button type="submit">Submit</button>
             </form>
         </div>
